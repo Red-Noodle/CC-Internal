09550e471606200a33d21e5e27c002269e9ede1c
Alex Rael <alprael22@gmail.com>
2019-10-29 17:30:25 -0600

    adding various variables

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,12 @@
+const error = require('http-errors');
+
 const express = require('express');
 const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const passport = require('passport');
+const Strategy = require('passport-local').Stretegy;
+const session = require('express-session');
+const flash = require('connect-flash');
+
+const db = MongoClient.connect('mongodb://localhost');
