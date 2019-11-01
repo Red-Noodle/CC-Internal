f4fd744e64c332e206c269f1a049d27882231099
Alex Rael <alprael22@gmail.com>
2019-11-01 00:02:57 -0600

    finally connected db

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,26 @@
 const express = require('express');
-const app = express();
-
-
-const PORT = process.env.PORT || 5000;
-
 const passport = require('passport');
 const Strategy = require('passport-local').Stretegy;
 const session = require('express-session');
 const flash = require('connect-flash');
 const mongoose = require('mongoose');
 
+const app = express();
 
-mongoose.connect('mongodb://localhost:27017//cc_admin', { useUnifiedTopology: true, useNewUrlParser: true })
+
+const PORT = process.env.PORT || 5000;
+
+
+
+mongoose.createConnection('mongodb://localhost/cc_admin', { useUnifiedTopology: true, useNewUrlParser: true })
 .then(() => console.log('DB connected...'))
 .catch((err) => {console.log(err)});
 
 //Routes
-app.use('/', require('./backend/routes/index'));
-app.use('/students', require('./backend/routes/students'));
-app.use('/instructors', require('./backend/routes/instructors'));
-app.use('/cohorts', require('./backend/routes/cohorts'));
+app.use('/', require('./routes/index'));
+app.use('/students', require('./routes/students'));
+app.use('/instructors', require('./routes/instructors'));
+app.use('/cohorts', require('./routes/cohorts'));
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
